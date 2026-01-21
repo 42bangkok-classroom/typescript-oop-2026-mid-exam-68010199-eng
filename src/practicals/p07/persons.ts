026e932614730e3db4c1146922a683ce91e862b1
unknown <68010199@kmitl.ac.th>
2026-01-21 15:32:04 +0700

    Updates

--- src/practicals/p07/persons.ts
+++ src/practicals/p07/persons.ts
@@ -1,10 +1,10 @@
-interface Person {
+export interface Person {
   firstName?: string;
   lastName?: string;
   age?: number;
 }
 
-function sortPersons(persons: Person[]): Person[] {
+export function sortPersons(persons: Person[]): Person[] {
   return persons
     .filter(
       p =>
@@ -17,21 +17,10 @@ function sortPersons(persons: Person[]): Person[] {
         p.age <= 120
     )
     .sort((a, b) => {
-      if (a.firstName! < b.firstName!) return -1
-      if (a.firstName! > b.firstName!) return 1
-      if (a.lastName! < b.lastName!) return -1
-      if (a.lastName! > b.lastName!) return 1
-      return a.age! - b.age!
-    })
+      if (a.firstName! < b.firstName!) return -1;
+      if (a.firstName! > b.firstName!) return 1;
+      if (a.lastName! < b.lastName!) return -1;
+      if (a.lastName! > b.lastName!) return 1;
+      return a.age! - b.age!;
+    });
 }
-
-
-const persons: Person[] = [
-  { firstName: "Somchai", lastName: "Jaidee", age: 30 },
-  { firstName: "", lastName: "Test", age: 20 },
-  { firstName: "Anan", lastName: "Sukjai", age: 25 },
-  { firstName: "Somchai", lastName: "Dee", age: 25 },
-  { firstName: "John", lastName: "Doe", age: 150 },
-  { firstName: "Jane", lastName: "Smith", age: -5 },
-  { lastName: "NoName", age: 40 }
-];
