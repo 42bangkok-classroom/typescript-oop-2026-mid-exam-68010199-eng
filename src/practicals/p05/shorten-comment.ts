ca0107dbed16bb2c10618e73a243c2406de7ff43
unknown <68010199@kmitl.ac.th>
2026-01-21 15:49:52 +0700

    Updates

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -2,6 +2,5 @@ export function shortenComment(comment: string): string {
   return comment
     .split(" ")
     .filter(word => word.length >= 5 && word.length <= 10)
-    .map(word => word[0])
-    .join("");
+    .join(" ");
 }
