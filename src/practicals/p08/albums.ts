92ff46fb18c1dcac3e07824b0c0113b71c1bb1d9
unknown <68010199@kmitl.ac.th>
2026-01-21 15:56:07 +0700

    Updates

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -8,18 +8,18 @@ export interface FullAlbum {
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-export function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
   return Promise.all([
     fetch(ALBUMS_URL).then(res => res.json()),
     fetch(PHOTOS_URL).then(res => res.json())
   ]).then(([albums, photos]) => {
-    let result: FullAlbum[] = albums.map((album: any) => ({
+    let result = albums.map((album: any) => ({
       ...album,
       photos: photos.filter((p: any) => p.albumId === album.id)
     }))
 
     if (userIds && userIds.length > 0) {
-      result = result.filter(a => userIds.includes(a.userId))
+      result = result.filter((a: any) => userIds.includes(a.userId))
     }
 
     return result
