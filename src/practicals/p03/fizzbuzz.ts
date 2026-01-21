07f76b837fa897062a20de567dd3d05c2f8e5cf3
unknown <68010199@kmitl.ac.th>
2026-01-21 14:46:22 +0700

    Updates

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,6 +1,19 @@
-export function fizzbuzz(n: number): string {
-  if (n % 15 === 0) return "FizzBuzz"
-  if (n % 3 === 0) return "Fizz"
-  if (n % 5 === 0) return "Buzz"
-  return n.toString()
+export function fizzbuzz(input?: string): void {
+  const n = Number(input)
+
+  if (!input || isNaN(n) || n <= 0) {
+    return
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+      console.log("Fizz")
+    } else if (i % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(i)
+    }
+  }
 }
