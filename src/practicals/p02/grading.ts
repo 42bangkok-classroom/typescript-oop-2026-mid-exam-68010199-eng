ca1899353f962b310f94501fca206a622e5e932b
unknown <68010199@kmitl.ac.th>
2026-01-21 15:17:29 +0700

    Updates

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -3,16 +3,16 @@ declare const process: any
 const input = process.argv[2]
 const score = Number(input)
 
-if (!input || isNaN(score) || score < 0 || score > 100) {
+if (input === undefined || isNaN(score) || score < 0 || score > 100) {
   console.log("Invalid input")
 } else if (score < 50) {
-  console.log("Grade is F")
+  console.log("Grade F")
 } else if (score < 60) {
-  console.log("Grade is D")
+  console.log("Grade D")
 } else if (score < 70) {
-  console.log("Grade is C")
+  console.log("Grade C")
 } else if (score < 80) {
-  console.log("Grade is B")
+  console.log("Grade B")
 } else {
-  console.log("Grade is A")
+  console.log("Grade A")
 }
