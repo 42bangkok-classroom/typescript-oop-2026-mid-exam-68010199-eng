45d0bbab57f5fd43d7619f95e57e3e6ae1cfa1fb
unknown <68010199@kmitl.ac.th>
2026-01-21 15:14:20 +0700

    Updates

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,10 @@
-// Write your code below
+export function numberPattern(n: number): string {
+  let result = ""
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      result += j.toString()
+    }
+    if (i < n) result += "\n"
+  }
+  return result
+}
