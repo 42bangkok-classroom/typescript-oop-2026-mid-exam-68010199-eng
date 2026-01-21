7c7d2cda596f0ee126efbe96ebc967e0ffa7e692
unknown <68010199@kmitl.ac.th>
2026-01-21 15:34:20 +0700

    Updates

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,12 +1,20 @@
-function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
+export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   const set1 = new Set(arr1)
   const set2 = new Set(arr2)
 
-  return [
-    ...arr1.filter(n => !set2.has(n)),
-    ...arr2.filter(n => !set1.has(n))
-  ]
-}
+  const result = new Set<number>()
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+  for (const n of set1) {
+    if (!set2.has(n)) {
+      result.add(n)
+    }
+  }
+
+  for (const n of set2) {
+    if (!set1.has(n)) {
+      result.add(n)
+    }
+  }
+
+  return Array.from(result)
+}
