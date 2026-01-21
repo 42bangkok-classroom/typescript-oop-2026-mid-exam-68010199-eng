d54deb932eac456c864f697c13ff4144515ba6b2
unknown <68010199@kmitl.ac.th>
2026-01-21 15:01:29 +0700

    Updates

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,20 +1,18 @@
-export default function grading(input?: string): void {
-  const score = Number(input)
+declare const process: any
 
-  if (!input || isNaN(score) || score < 0 || score > 100) {
-    console.log("Invalid input")
-    return
-  }
+const input = process.argv[2]
+const score = Number(input)
 
-  if (score < 50) {
-    console.log("Grade is F")
-  } else if (score < 60) {
-    console.log("Grade is D")
-  } else if (score < 70) {
-    console.log("Grade is C")
-  } else if (score < 80) {
-    console.log("Grade is B")
-  } else {
-    console.log("Grade is A")
-  }
+if (!input || isNaN(score) || score < 0 || score > 100) {
+  console.log("Invalid input")
+} else if (score < 50) {
+  console.log("Grade is F")
+} else if (score < 60) {
+  console.log("Grade is D")
+} else if (score < 70) {
+  console.log("Grade is C")
+} else if (score < 80) {
+  console.log("Grade is B")
+} else {
+  console.log("Grade is A")
 }
