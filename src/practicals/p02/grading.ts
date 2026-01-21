fe0b08a0cc45efce5c37f8374f2a8bbaa246dadf
unknown <68010199@kmitl.ac.th>
2026-01-21 14:50:24 +0700

    Updates

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,13 +1,20 @@
-export function grading(score: number): string {
-  if (score >= 80) {
-    return "A"
-  } else if (score >= 70) {
-    return "B"
-  } else if (score >= 60) {
-    return "C"
-  } else if (score >= 50) {
-    return "D"
+export function grading(input?: string): void {
+  const score = Number(input)
+
+  if (!input || isNaN(score) || score < 0 || score > 100) {
+    console.log("Invalid input")
+    return
+  }
+
+  if (score < 50) {
+    console.log("Grade is F")
+  } else if (score < 60) {
+    console.log("Grade is D")
+  } else if (score < 70) {
+    console.log("Grade is C")
+  } else if (score < 80) {
+    console.log("Grade is B")
   } else {
-    return "F"
+    console.log("Grade is A")
   }
 }
