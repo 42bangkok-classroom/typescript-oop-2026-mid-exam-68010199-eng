ba9be39be5d721b2c979cb723cf06c80e5292b68
unknown <68010199@kmitl.ac.th>
2026-01-21 13:53:42 +0700

    Updates

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,13 @@
-// Write your code below
+export function grading(score: number): string {
+  if (score >= 80) {
+    return "A"
+  } else if (score >= 70) {
+    return "B"
+  } else if (score >= 60) {
+    return "C"
+  } else if (score >= 50) {
+    return "D"
+  } else {
+    return "F"
+  }
+}
